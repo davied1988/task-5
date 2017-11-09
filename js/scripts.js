2783cbaf3383dcff77901c6b8ba8d86e2ac7c0f0
Dawid G <dawid.grudziecki@gmail.com>
2017-11-09 18:08:47 +0100

    task 5 files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var a = 7,
+	b = 12,
+	value = (a * a) - (2 * a * b) + (b * b);
+	
+console.log(value);
+
+if (value > 0) {
+    console.log('wynik dodatni');
+} else if (value < 0 ) {
+    console.log('wynik ujemny');
+} else {
+	console.log('wynik równy 0');
+}
